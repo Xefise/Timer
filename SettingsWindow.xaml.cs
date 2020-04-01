977d098f8aee1c32071d29ee310f16f3bb276b93
Xefise <Xefise@gmail.com>
2020-04-01 11:57:16 +0300

    Remove useless code

--- SettingsWindow.xaml.cs
+++ SettingsWindow.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Windows;
-using System.Windows.Media;
+﻿using System.Windows;
 
 namespace Timer
 {
@@ -19,11 +17,6 @@ namespace Timer
             Properties.Settings.Default.DarkTheme = CB_Theme.IsChecked ?? false;
             Properties.Settings.Default.UnitInSeconds = CB_UnitInSeconds.IsChecked ?? false;
             Properties.Settings.Default.Save();
-
-            if (!CB_Theme.IsChecked ?? false) MainWindow.Theme = "Themes/Light"; else MainWindow.Theme = "Themes/Dark";
-            ResourceDictionary resourceDict = Application.LoadComponent(new Uri($"{MainWindow.Theme}.xaml", UriKind.Relative)) as ResourceDictionary;
-            Application.Current.Resources.Clear();
-            Application.Current.Resources.MergedDictionaries.Add(resourceDict);
 
             MainWindow.UpdateTheme();
             MainWindow.UnitInSeconds = CB_UnitInSeconds.IsChecked ?? false;
